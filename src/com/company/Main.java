7dc714a72dc12455f2e19e08f50910be44bd6cec
shea.mackie <shea.mackie@vashonsd.org>
2021-10-05 11:25:13 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print(squareOf(4));
+    }
+    public static int squareOf(int number){
+        number *= number;
+        return number;
+    }
+}
